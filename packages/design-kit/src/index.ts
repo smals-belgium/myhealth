8d7afc8e7f48b45494168bf507b663038c346c9e
Esteban Beerens <esteban.beerens@gmail.com>
2026-06-29 10:47:00 +0200

    feat(chip): create chip component with variants

--- packages/design-kit/src/index.ts
+++ packages/design-kit/src/index.ts
@@ -1,6 +1,7 @@
 export * from './button';
 export * from './card';
 export * from './checkbox';
+export * from './chip';
 export * from './dialog';
 export * from './icon-button';
 export * from './divider';
